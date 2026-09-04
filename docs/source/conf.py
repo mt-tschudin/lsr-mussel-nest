cdbb26fdd65dc007b676b7039ef82e876d8fa2eb
mt-tschudin <94310921+mt-tschudin@users.noreply.github.com>
2026-09-04 14:54:18 +0200

    Update project metadata in Sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = "LSR Mussel Nest"
+copyright = "2026, Laboratory of Sustainability Robotics"
+author = "Laboratory of Sustainability Robotics"
 
-release = '0.1'
-version = '0.1.0'
+release = "1.0"
 
 # -- General configuration
 

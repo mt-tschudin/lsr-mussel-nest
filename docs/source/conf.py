af2b9598ad0b8fd0e5eb4fba1bbc0bd4667903e4
mt-tschudin <94310921+mt-tschudin@users.noreply.github.com>
2026-09-04 15:35:06 +0200

    Add HTML theme options for Sphinx documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,12 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 3,
+}
+
 html_static_path = ["_static"]
 
 html_css_files = [

fe311e11684bb10af585ddf08b0ae0209811f0e7
mt-tschudin <94310921+mt-tschudin@users.noreply.github.com>
2026-09-04 14:44:27 +0200

    Add static path and custom CSS for HTML output

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,11 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+html_static_path = ["_static"]
+
+html_css_files = [
+    "custom.css",
+]
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
